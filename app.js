d74a6302d9ed4c87a293c18d418d5cafb2ae9bca
lgarciabode <lgarciabode@gamil.com>
2025-01-24 15:05:39 -0300

    Agregado de funciones al boton Sortear amigo

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 // Creacion de array
-const amigos = [];
+let amigos = [];
+//Boton para el sorteo
+const botonSortear = document.getElementById('sorteo');
+const iconoSorteo = document.getElementById('iconoSorteo');
 
 //FUNCIONES
 //Funcion para agregar amigos
@@ -29,20 +32,19 @@ function agregarAmigo() {
 //Funcion para actualizar la lista de amigos
 function actualizarLista() {
     const lista = document.getElementById('listaAmigos');
-    //Limpia la lista antes de actualizarla
-    lista.innerHTML = '';
-
-    //Recorre el array de amigos y crea un elemento li
+    //Recorre el array de amigos y crea unelemento li
     //para cada uno
-    amigos.forEach(amigo => {
-        const li = document.createElement('li');
-        li.textContent = amigo;
-        lista.appendChild(li);
-    })
+    lista.innerHTML = amigos.map(amigo => `<li>${amigo}</li>`).join('');
 }
 
 //Funcion para sortear amigo invisible
 function sortearAmigo() {
+    //Evaluacion del estado del boton sortear
+    if (botonSortear.dataset.estado === "reiniciar") {
+        reiniciarSorteo();
+        return;
+    }
+
     //Valida que haya amigos en la lista
     if (amigos.length === 0) {
         alert('No hay amigos para sortear, añade algunos');
@@ -57,5 +59,25 @@ function sortearAmigo() {
 
     //Muestra el resultado
     const resultado = document.getElementById('resultado');
-    resultado.innerHTML = `El amigo sorteado es: ${amigoSorteado}`
+    resultado.innerHTML = `El amigo sorteado es: ${amigoSorteado}`;
+
+    //Cambia el texto del boton y actuliza su estado
+    iconoSorteo.src = "assets/reiniciar_icono.png"
+    botonSortear.innerHTML = "Reiniciar sorteo";
+    botonSortear.dataset.estado = "reiniciar";
 }
+
+//Funcion para reiniciar el sorteo, agregando nuevos amigos
+function reiniciarSorteo() {
+    //Limpia el array de amigos
+    amigos.length = 0;
+    //Limpia la lista de resultados
+    document.getElementById('listaAmigos').innerHTML = '';
+    document.getElementById('resultado').innerHTML = '';
+
+    //Restaurar nombre y estado del boton 
+    iconoSorteo.src = "assets/play_circle_outline.png"
+    botonSortear.innerHTML = "Sortear amigo";
+    botonSortear.dataset.estado = "sortear";
+}
+
